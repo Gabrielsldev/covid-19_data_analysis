e1085050cdb3bfc5a137286f755151acf4ea98f9
Gabrielsldev <gabrielsl.dev@gmail.com>
2021-01-21 22:50:25 -0300

    Adicionado novas funções de gráficos

--- COVID-19/covid-19.py
+++ COVID-19/covid-19.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import numpy as np
+
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
 import geopandas as gpd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+
 
 import os
 
@@ -15,7 +19,11 @@ if not os.path.exists("imagens"):
 df = pd.read_csv("HIST_PAINEL_COVIDBR_19jan2021.csv", sep=";")
 df_copy = df.copy()
 
+# Pega a última data disponível no dataset e atribui a uma variável que será usada em algumas funções.
 ultima_data = df_copy["data"].iloc[-1]
+
+
+# Cria um dataframe com os casos acumulados no Brasil
 
 
 def CasosAcumuladosBr():
@@ -27,15 +35,21 @@ def CasosAcumuladosBr():
     return casos_acumulados_br
 
 
-fig = px.line(x=CasosAcumuladosBr().index,
-              y=CasosAcumuladosBr()["Casos Acumulados (Brasil)"],
-              title='Casos Acumulados de COVID-19 no Brasil')
-fig.update_layout(
-    xaxis_title="Data",
-    yaxis_title="Casos Acumulados (Brasil)")
+# Cria um gráfico de linha com os casos acumulados no país.
 
 
-fig.show()
+def GraficoCasosAcumBR():
+    fig = px.line(x=CasosAcumuladosBr().index,
+                  y=CasosAcumuladosBr()["Casos Acumulados (Brasil)"],
+                  title='Casos Acumulados de COVID-19 no Brasil')
+    fig.update_layout(
+        xaxis_title="Data",
+        yaxis_title="Casos Acumulados (Brasil)")
+
+    return fig.show()
+
+# Cria um dataframe com os novos casos no Brasil.
+
 
 def CasosNovosBr():
     casos_novos_br = df_copy[["regiao", "casosNovos", "data"]]
@@ -45,44 +59,70 @@ def CasosNovosBr():
 
     return casos_novos_br
 
-fig = px.bar(x=CasosNovosBr().index, y=CasosNovosBr()["Casos Novos (Brasil)"],
-             title='Casos Novos de COVID-19 no Brasil')
-fig.update_layout(
-    xaxis_title="Data",
-    yaxis_title="Casos Novos (Brasil)")
 
-fig.show()
+# Cria um gráfico de linha com os novos casos no país.
 
 
-def ObitosAcumuladosBr():
-    obitos_acumulados_br = df_copy[["estado", "obitosAcumulado", "data"]]
-    obitos_acumulados_br = obitos_acumulados_br[
-        obitos_acumulados_br["data"] == ultima_data].rename(
-        columns={"obitosAcumulado":"Óbitos Acumulados (Brasil)", "data":"Data"})
-    obitos_acumulados_br.set_index("Data", inplace=True)
-    obitos_acumulados_br = obitos_acumulados_br.iloc[1:28].sort_values(by=["estado"])
+def GraficoCasosNovosBR():
+    fig = px.bar(x=CasosNovosBr().index, y=CasosNovosBr()["Casos Novos (Brasil)"],
+                 title='Casos Novos de COVID-19 no Brasil')
+    fig.update_layout(
+        xaxis_title="Data",
+        yaxis_title="Casos Novos (Brasil)")
 
-    return obitos_acumulados_br
-
-info_estados = gpd.read_file("bcim_2016_21_11_2018.gpkg", layer="lim_unidade_federacao_a")
-info_estados.rename({"sigla":"estado"}, axis=1, inplace=True)
-obitos_estados = info_estados.merge(ObitosAcumuladosBr(), on="estado", how="left")
+    return fig.show()
 
 
-obitos_estados.plot(column="Óbitos Acumulados (Brasil)",
-             cmap="Reds",
-             legend=True,
-             figsize=(32,20),
-             edgecolor="black")
+# Cria um dataframe com os óbitos acumulados por estado.
 
-plt.savefig('imagens/obitos_por_estado_mapa.png', dpi=300)
 
-fig = px.bar(ObitosAcumuladosBr(), x=ObitosAcumuladosBr()["estado"], y=ObitosAcumuladosBr()["Óbitos Acumulados (Brasil)"], title="Óbitos Acumulados (Brasil)")
-fig.update_layout(
-    xaxis_title="Data",
-    yaxis_title="Casos Novos (Brasil)")
+def ObitosAcumuladosUF():
+    obitos_acumulados_UF = df_copy[["estado", "obitosAcumulado", "data"]]
+    obitos_acumulados_UF = obitos_acumulados_UF[
+        obitos_acumulados_UF["data"] == ultima_data].rename(
+        columns={"obitosAcumulado":"Óbitos Acumulados (Estados)", "data":"Data"})
+    obitos_acumulados_UF.set_index("Data", inplace=True)
+    obitos_acumulados_UF = obitos_acumulados_UF.iloc[1:28].sort_values(by=["estado"])
 
-fig.show()
+    return obitos_acumulados_UF
+
+
+# Cria um gráfico do mapa do Brasil com os óbitos acumulados por estado.
+
+
+def MapaObitosAcumUF():
+    info_estados = gpd.read_file("bcim_2016_21_11_2018.gpkg", layer="lim_unidade_federacao_a")
+    info_estados.rename({"sigla":"estado"}, axis=1, inplace=True)
+    obitos_estados = info_estados.merge(ObitosAcumuladosUF(), on="estado", how="left")
+
+
+    obitos_estados.plot(column="Óbitos Acumulados (Estados)",
+                 cmap="Reds",
+                 legend=True,
+                 figsize=(32,20),
+                 edgecolor="black")
+
+    # plt.savefig('imagens/obitos_por_estado_mapa.png', dpi=300)
+
+    return plt.savefig('imagens/obitos_por_estado_mapa.png', dpi=300)
+        # plt.show()
+
+
+# Cria um gráfico de barras com os óbitos acumulados por estado.
+
+
+def GraficoObitosAcumUF():
+    fig = px.bar(ObitosAcumuladosUF(), x=ObitosAcumuladosUF()["estado"],
+                 y=ObitosAcumuladosUF()["Óbitos Acumulados (Estados)"], title="Óbitos Acumulados (Estados)")
+    fig.update_layout(
+        xaxis_title="Data",
+        yaxis_title="Óbitos Acumulados (Estados)")
+
+    return fig.show()
+
+
+# Cria um dataframe com o número de casos acumulados por estado, levando em conta a população.
+
 
 def CasosPorPop():
     casos_por_pop = df_copy[["estado", "populacaoTCU2019","casosAcumulado", "data"]]
@@ -95,30 +135,77 @@ def CasosPorPop():
 
     return casos_por_pop
 
-# Regressão linear
-X = CasosPorPop()["População Estado"]
-Y = CasosPorPop()["Casos Acumulados (por estado)"]
 
-linear_regressor = LinearRegression()  # create object for the class
+# Cria um scatter plot com número de casos acumulados por estado, levando em conta a população.
 
-X = X.values.reshape(-1, 1)  # values converts it into a numpy array
-Y = Y.values.reshape(-1, 1)  # -1 means that calculate the dimension of rows, but have 1 column
 
-linear_regressor.fit(X, Y)  # perform linear regression
-Y_pred = linear_regressor.predict(X)  # make predictions
-
-fig = px.scatter(x=CasosPorPop()["População Estado"], y=CasosPorPop()["Casos Acumulados (por estado)"],
+def GraficoCasosPop():
+    fig = px.scatter(x=CasosPorPop()["População Estado"], y=CasosPorPop()["Casos Acumulados (por estado)"],
                  color=CasosPorPop()["estado"], size=CasosPorPop()["Casos Acumulados (por estado)"])
 
-fig.update_layout(
+    fig.update_layout(
     xaxis_title="População",
     yaxis_title="Casos Acumulados (por estado)")
 
-fig.show()
+    return fig.show()
 
-CasosPorPop().plot(x="População Estado", y="Casos Acumulados (por estado)",
+
+# Cria uma regrassão linear simples entre número de casos e população
+
+
+def RegLinCasosPop():
+    # Regressão linear
+    X = CasosPorPop()["População Estado"]
+    Y = CasosPorPop()["Casos Acumulados (por estado)"]
+
+    linear_regressor = LinearRegression()  # create object for the class
+
+    X = X.values.reshape(-1, 1)  # values converts it into a numpy array
+    Y = Y.values.reshape(-1, 1)  # -1 means that calculate the dimension of rows, but have 1 column
+
+    linear_regressor.fit(X, Y)  # perform linear regression
+    Y_pred = linear_regressor.predict(X)  # make predictions
+
+    CasosPorPop().plot(x="População Estado", y="Casos Acumulados (por estado)",
                kind="scatter", title="Casos por estado x População (Regressão Linear)")
-plt.scatter(X, Y)
-plt.plot(X, Y_pred, color='red')
+    plt.scatter(X, Y)
+    plt.plot(X, Y_pred, color='red')
+    plt.savefig('imagens/reg_linear_casos_por_pop.png', dpi=300)
 
-plt.savefig('imagens/reg_linear_casos_por_pop.png', dpi=300)
+    # fig.write_image("imagens/casos_por_pop.png", width=3840, height=2160)
+
+    return fig.write_image("imagens/casos_por_pop.png", width=3840, height=2160)
+        # plt.show()
+
+
+# Comparação entre número de casos e número de óbitos por estado.
+
+def Comparativo():
+    comparativo_df = CasosPorPop().merge(ObitosAcumuladosUF(), on="estado", how="left")
+    comparativo_df["Casos/População"] = comparativo_df["Casos Acumulados (por estado)"] / comparativo_df[
+        "População Estado"]
+    comparativo_df["Óbitos/Casos"] = comparativo_df["Óbitos Acumulados (Estados)"] / comparativo_df[
+        "Casos Acumulados (por estado)"]
+
+    return comparativo_df
+
+
+# Cria dois graicos com número de casos e número de óbitos por estado.
+
+
+def GraficoComparacao():
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.05)
+
+    fig.add_trace(go.Bar(x=Comparativo()["estado"], y=Comparativo()["Casos/População"],
+                         name="Casos/População"), row=1, col=1)
+
+    fig.add_trace(go.Bar(x=Comparativo()["estado"], y=Comparativo()["Óbitos/Casos"],
+                         name="Óbitos/Casos"), row=2, col=1)
+
+    fig.update_layout(height=600, width=800, title_text="Casos/População e Óbitos/Casos")
+    return fig.show()
+
+
+ObitosAcumuladosUF()
+Comparativo()
+GraficoComparacao()
